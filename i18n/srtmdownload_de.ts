365c39692ceb61aae2aceb74a22aef38c8365506
Horst <hdus@sourcepole.ch>
2018-01-01 11:59:21 +0100

    i18n added

---
+++ i18n/srtmdownload_de.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_CH">
+<context>
+    <name>SrtmDownloader</name>
+    <message>
+        <location filename="../srtm_downloader.py" line="173"/>
+        <source>&amp;SRTM Downloader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader.py" line="163"/>
+        <source>SRTM Downloader</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SrtmDownloaderDialogBase</name>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="14"/>
+        <source>SRTM Downloader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="22"/>
+        <source>North</source>
+        <translation>Nord</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="39"/>
+        <source>West</source>
+        <translation>West</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="56"/>
+        <source>East</source>
+        <translation>Ost</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="73"/>
+        <source>South</source>
+        <translation>Süd</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="98"/>
+        <source>Set canvas extent</source>
+        <translation>Kartenausdehnung setzen</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="105"/>
+        <source>Download</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="119"/>
+        <source>Output-Path:</source>
+        <translation>Ausgabepfad</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="129"/>
+        <source>select</source>
+        <translation>Wählen</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.py" line="119"/>
+        <source>Open Directory</source>
+        <translation>Verueichnis öffnen</translation>
+    </message>
+</context>
+</TS>
