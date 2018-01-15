721b9a9a78d4279cff1e75b131530b63e63fb0f3
Horst Düster <hdus@sourcepole.ch>
2018-01-15 17:26:54 +0100

    german translation improved

--- i18n/srtmdownload_de.ts
+++ i18n/srtmdownload_de.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de_CH">
+<TS version="2.1" language="de_CH">
 <context>
     <name>About</name>
     <message>
         <location filename="../about/do_about.py" line="36"/>
-        <source>About </source>
-        <translation></translation>
+        <source>About %s %s</source>
+        <translation>Über</translation>
     </message>
     <message>
         <location filename="../about/do_about.py" line="37"/>
-        <source>Version: </source>
-        <translation>Version: </translation>
+        <source>Version: %s</source>
+        <translation>Version: %s</translation>
     </message>
     <message>
         <location filename="../about/do_about.py" line="39"/>
@@ -35,8 +35,8 @@
     </message>
     <message>
         <location filename="../about/do_about.py" line="47"/>
-        <source>&lt;p&gt;</source>
-        <translation></translation>
+        <source>&lt;p&gt;%s&lt;br&gt;</source>
+        <translation>&lt;p&gt;%s&lt;br&gt;</translation>
     </message>
     <message>
         <location filename="../about/do_about.py" line="49"/>
@@ -57,47 +57,22 @@
 </source>
         <translation>Kontakt:</translation>
     </message>
-</context>
-<context>
-    <name>Login</name>
     <message>
-        <location filename="../login.ui" line="14"/>
-        <source>Login</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../login.ui" line="23"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you don&apos;t have an Earth Data Account, you can create one  &lt;a href=&quot;https://urs.earthdata.nasa.gov/home&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wenn Sie keinen Earth Data Account haben, können Sie &lt;br&gt;&lt;a href=&quot;https://urs.earthdata.nasa.gov/home&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;hier&lt;/span&gt;&lt;/a&gt; einen erstellen.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-    <message>
-        <location filename="../login.ui" line="33"/>
-        <source>Username:</source>
-        <translation>Benutzername:</translation>
-    </message>
-    <message>
-        <location filename="../login.ui" line="43"/>
-        <source>Password:</source>
-        <translation>Passwort:</translation>
-    </message>
-</context>
-<context>
-    <name>RegistrationView</name>
-    <message>
-        <location filename="../registration_view.ui" line="14"/>
-        <source>Earthdata Login Registration</source>
-        <translation>Earth Data Konto Registrierung</translation>
+        <location filename="../about/do_about.py" line="56"/>
+        <source>Plugin Resources:
+</source>
+        <translation>Plugin Quellen:</translation>
     </message>
 </context>
 <context>
     <name>SrtmDownloader</name>
     <message>
-        <location filename="../srtm_downloader.py" line="174"/>
+        <location filename="../srtm_downloader.py" line="65"/>
         <source>&amp;SRTM Downloader</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader.py" line="164"/>
+        <location filename="../srtm_downloader.py" line="161"/>
         <source>SRTM Downloader</source>
         <translation></translation>
     </message>
@@ -105,104 +80,84 @@
 <context>
     <name>SrtmDownloaderDialogBase</name>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="14"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="20"/>
         <source>SRTM Downloader</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="209"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="212"/>
         <source>North</source>
         <translation>Nord</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="29"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="38"/>
         <source>West</source>
         <translation>West</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="53"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="62"/>
         <source>East</source>
         <translation>Ost</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="185"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="188"/>
         <source>South</source>
         <translation>Süd</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="68"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="77"/>
         <source>Set canvas extent</source>
         <translation>Kartenausdehnung setzen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="238"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="134"/>
         <source>Download</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="147"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="163"/>
         <source>Output-Path:</source>
         <translation>Ausgabepfad</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="161"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="177"/>
         <source>select</source>
         <translation>Wählen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="195"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="187"/>
         <source>Open Directory</source>
         <translation>Verueichnis öffnen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="105"/>
-        <source>Over all progress</source>
-        <translation>Fortschritt über alle Bilder</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="131"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="147"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="154"/>
-        <source>[empty for temporary file]</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="224"/>
-        <source>Image download progress</source>
-        <translation>Fortschritt beim Herunterladen</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="245"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="26"/>
         <source>Load Images to QGIS</source>
         <translation>Bilder in die Karte laden</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="164"/>
-        <source>Error</source>
-        <translation>Fehler</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="158"/>
-        <source>Earth Data Authentication Error!</source>
-        <translation>Earth Data Anmeldefehler!</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="164"/>
-        <source>HTTP-Error: %s</source>
-        <translation>HTTP-Fehler: %s</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="169"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="171"/>
         <source>Result</source>
         <translation>Ergebnis</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="169"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="171"/>
         <source>Download completed</source>
         <translation>Herunterladen der Bilder erfolgreich</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="170"/>
+        <source>[empty for temporary files]</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.py" line="205"/>
+        <source>Download-Progress: %s of %s images</source>
+        <translation>Fortschritt beim Herunterladen: %s von %s Bildern</translation>
     </message>
 </context>
 </TS>
