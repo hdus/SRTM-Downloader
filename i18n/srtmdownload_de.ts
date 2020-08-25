2cddb7d99be701a169d8ae5f69ef168032f6b571
Horst <hdus@sourcepole.ch>
2020-08-25 17:12:42 +0200

    german translation improved

--- i18n/srtmdownload_de.ts
+++ i18n/srtmdownload_de.ts
@@ -1,78 +1,106 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_CH">
+<TS version="2.0" language="de_CH">
 <context>
     <name>About</name>
     <message>
-        <location filename="../about/do_about.py" line="36"/>
-        <source>About %s %s</source>
-        <translation>Über</translation>
-    </message>
-    <message>
-        <location filename="../about/do_about.py" line="37"/>
-        <source>Version: %s</source>
-        <translation>Version: %s</translation>
-    </message>
-    <message>
-        <location filename="../about/do_about.py" line="39"/>
+        <location filename="../about/do_about.py" line="38"/>
         <source>Author</source>
         <translation>Autor</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="40"/>
+        <location filename="../about/do_about.py" line="39"/>
         <source>Contact</source>
         <translation>Kontakt</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="41"/>
+        <location filename="../about/do_about.py" line="40"/>
         <source>Change Log</source>
         <translation>Änderungen</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="46"/>
+        <location filename="../about/do_about.py" line="45"/>
         <source>&lt;p&gt;&lt;center&gt;&lt;b&gt;Author(s):&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</source>
         <translation>&lt;p&gt;&lt;center&gt;&lt;b&gt;Autor(en):&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="47"/>
-        <source>&lt;p&gt;%s&lt;br&gt;</source>
-        <translation>&lt;p&gt;%s&lt;br&gt;</translation>
-    </message>
-    <message>
-        <location filename="../about/do_about.py" line="49"/>
+        <location filename="../about/do_about.py" line="48"/>
         <source>Sourcepole AG - Linux &amp; Open Source Solutions
 </source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="50"/>
+        <location filename="../about/do_about.py" line="49"/>
         <source>Weberstrasse 5, 8004 Zurich, Switzerland
 </source>
         <translation>Weberstrasse 5, 8004 Zürich, Schweiz
 </translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="53"/>
+        <location filename="../about/do_about.py" line="52"/>
         <source>Contact:
 </source>
         <translation>Kontakt:</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="56"/>
+        <location filename="../about/do_about.py" line="55"/>
         <source>Plugin Resources:
 </source>
         <translation>Plugin Quellen:</translation>
+    </message>
+    <message>
+        <location filename="../about/do_about.py" line="35"/>
+        <source>About {0} {1}</source>
+        <translation>Über {0}  {1}</translation>
+    </message>
+    <message>
+        <location filename="../about/do_about.py" line="36"/>
+        <source>Version: {0}</source>
+        <translation>Version: {0}</translation>
+    </message>
+    <message>
+        <location filename="../about/do_about.py" line="46"/>
+        <source>&lt;p&gt;{0}&lt;br&gt;</source>
+        <translation>&lt;p&gt;{0}&lt;br&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Login</name>
+    <message>
+        <location filename="../srtm_downloader_login.ui" line="14"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_login.ui" line="23"/>
+        <source>Password:</source>
+        <translation>Passwort:</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_login.ui" line="30"/>
+        <source>Username:</source>
+        <translation>Benutzername:</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_login.ui" line="44"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;https://urs.earthdata.nasa.gov is requesting your username and password. If you do not have a Earthdata Login, create one at &lt;/p&gt;&lt;p&gt;&lt;a href=&quot;https://urs.earthdata.nasa.gov//users/new&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://urs.earthdata.nasa.gov//users/new&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_login.ui" line="80"/>
+        <source>Save Credentials</source>
+        <translation>Anmeldeinformationen speichern</translation>
     </message>
 </context>
 <context>
     <name>SrtmDownloader</name>
     <message>
-        <location filename="../srtm_downloader.py" line="65"/>
+        <location filename="../srtm_downloader.py" line="66"/>
         <source>&amp;SRTM Downloader</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader.py" line="161"/>
+        <location filename="../srtm_downloader.py" line="162"/>
         <source>SRTM Downloader</source>
         <translation></translation>
     </message>
@@ -85,79 +113,89 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="212"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="37"/>
         <source>North</source>
         <translation>Nord</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="38"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="54"/>
         <source>West</source>
         <translation>West</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="62"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="78"/>
         <source>East</source>
         <translation>Ost</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="188"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="95"/>
         <source>South</source>
         <translation>Süd</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="77"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="108"/>
         <source>Set canvas extent</source>
         <translation>Kartenausdehnung setzen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="134"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="159"/>
         <source>Download</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="163"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="191"/>
         <source>Output-Path:</source>
         <translation>Ausgabepfad</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="177"/>
-        <source>select</source>
-        <translation>Wählen</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="187"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="196"/>
         <source>Open Directory</source>
-        <translation>Verueichnis öffnen</translation>
+        <translation>Verzeichnis öffnen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="147"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="125"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="26"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="149"/>
         <source>Load Images to QGIS</source>
         <translation>Bilder in die Karte laden</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="171"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="172"/>
         <source>Result</source>
         <translation>Ergebnis</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="171"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="172"/>
         <source>Download completed</source>
         <translation>Herunterladen der Bilder erfolgreich</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="170"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="198"/>
         <source>[empty for temporary files]</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="205"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="234"/>
         <source>Download-Progress: %s of %s images</source>
         <translation>Fortschritt beim Herunterladen: %s von %s Bildern</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="205"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.py" line="162"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.py" line="162"/>
+        <source>Wrong definition of coordinates</source>
+        <translation>Falsche Koordinatendefinition</translation>
     </message>
 </context>
 </TS>
