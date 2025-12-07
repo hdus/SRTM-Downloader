57a328f00127346b1fec702a4051b0427d1ea1ca
Horst <horst.duester@kappasys.ch>
2025-12-07 16:28:38 +0100

    german translation improved

--- i18n/srtmdownload_de.ts
+++ i18n/srtmdownload_de.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="de_CH" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="de_CH">
 <context>
     <name>About</name>
     <message>
@@ -23,19 +24,6 @@
         <translation>&lt;p&gt;&lt;center&gt;&lt;b&gt;Autor(en):&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="48"/>
-        <source>Sourcepole AG - Linux &amp; Open Source Solutions
-</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../about/do_about.py" line="49"/>
-        <source>Weberstrasse 5, 8004 Zurich, Switzerland
-</source>
-        <translation>Weberstrasse 5, 8004 Zürich, Schweiz
-</translation>
-    </message>
-    <message>
         <location filename="../about/do_about.py" line="52"/>
         <source>Contact:
 </source>
@@ -47,59 +35,16 @@
 </source>
         <translation>Plugin Quellen:</translation>
     </message>
-    <message>
-        <location filename="../about/do_about.py" line="35"/>
-        <source>About {0} {1}</source>
-        <translation>Über {0}  {1}</translation>
-    </message>
-    <message>
-        <location filename="../about/do_about.py" line="36"/>
-        <source>Version: {0}</source>
-        <translation>Version: {0}</translation>
-    </message>
-    <message>
-        <location filename="../about/do_about.py" line="46"/>
-        <source>&lt;p&gt;{0}&lt;br&gt;</source>
-        <translation>&lt;p&gt;{0}&lt;br&gt;</translation>
-    </message>
-</context>
-<context>
-    <name>Login</name>
-    <message>
-        <location filename="../srtm_downloader_login.ui" line="14"/>
-        <source>Login</source>
-        <translation>Login</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_login.ui" line="23"/>
-        <source>Password:</source>
-        <translation>Passwort:</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_login.ui" line="30"/>
-        <source>Username:</source>
-        <translation>Benutzername:</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_login.ui" line="44"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;https://urs.earthdata.nasa.gov is requesting your username and password. If you do not have a Earthdata Login, create one at &lt;/p&gt;&lt;p&gt;&lt;a href=&quot;https://urs.earthdata.nasa.gov//users/new&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://urs.earthdata.nasa.gov//users/new&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_login.ui" line="80"/>
-        <source>Save Credentials</source>
-        <translation>Anmeldeinformationen speichern</translation>
-    </message>
 </context>
 <context>
     <name>SrtmDownloader</name>
     <message>
-        <location filename="../srtm_downloader.py" line="66"/>
+        <location filename="../srtm_downloader.py" line="67"/>
         <source>&amp;SRTM Downloader</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader.py" line="162"/>
+        <location filename="../srtm_downloader.py" line="165"/>
         <source>SRTM Downloader</source>
         <translation></translation>
     </message>
@@ -107,94 +52,133 @@
 <context>
     <name>SrtmDownloaderDialogBase</name>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="20"/>
-        <source>SRTM Downloader</source>
-        <translation></translation>
+        <location filename="../srtm_downloader_dialog_base.py" line="165"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="157"/>
+        <source>Box out of covered area</source>
+        <translation>Box außerhalb des gedeckten Bereichs</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="30"/>
-        <source>North</source>
-        <translation>Nord</translation>
+        <location filename="../srtm_downloader_dialog_base.py" line="158"/>
+        <source>The area you have defined is completely outside the area covered by the SRTM tiles. </source>
+        <translation>Der von Ihnen definierte Bereich liegt vollständig außerhalb des von den SRTM-Kacheln abgedeckten Bereichs.</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="47"/>
-        <source>West</source>
-        <translation>West</translation>
+        <location filename="../srtm_downloader_dialog_base.py" line="166"/>
+        <source>The area you have defined is partly outside the area covered by the SRTM tiles. Do you like to continue?</source>
+        <translation>Der von Ihnen definierte Bereich liegt teilweise außerhalb des von den SRTM-Kacheln abgedeckten Gebiets. Möchten Sie fortfahren?</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="71"/>
-        <source>East</source>
-        <translation>Ost</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="88"/>
-        <source>South</source>
-        <translation>Süd</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="101"/>
-        <source>Set canvas extent</source>
-        <translation>Kartenausdehnung setzen</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="152"/>
-        <source>Download</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="184"/>
-        <source>Output-Path:</source>
-        <translation>Ausgabepfad</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="196"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="224"/>
         <source>Open Directory</source>
         <translation>Verzeichnis öffnen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="118"/>
-        <source>About</source>
-        <translation>Über</translation>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>SRTM Downloader</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="142"/>
-        <source>Load Images to QGIS</source>
-        <translation>Bilder in die Karte laden</translation>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>Load Image to QGIS</source>
+        <translation>DEM in QGIS laden</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="172"/>
-        <source>Result</source>
-        <translation>Ergebnis</translation>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>Download</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="172"/>
-        <source>Download completed</source>
-        <translation>Herunterladen der Bilder erfolgreich</translation>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>Output-Path:</source>
+        <translation>Ausgabepfad</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="191"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>[empty for temporary files]</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="234"/>
-        <source>Download-Progress: %s of %s images</source>
-        <translation>Fortschritt beim Herunterladen: %s von %s Bildern</translation>
-    </message>
-    <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="198"/>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="162"/>
-        <source>Error</source>
-        <translation>Fehler</translation>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>About</source>
+        <translation>Über</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="162"/>
-        <source>Wrong definition of coordinates</source>
-        <translation>Falsche Koordinatendefinition</translation>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>DEM-Type:</source>
+        <translation>DEM-Typ:</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>South</source>
+        <translation>Süd</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>Set canvas extent</source>
+        <translation>Kartenausdehnung setzen</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>East</source>
+        <translation>Ost</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>North</source>
+        <translation>Nord</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>West</source>
+        <translation>West</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>Request API-Key from OpenTopography Data</source>
+        <translation>API-Schlüssel von OpenTopography Data anfordern</translation>
+    </message>
+    <message>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>&lt;a href=&quot;https://opentopography.org/blog/introducing-api-keys-access-opentopography-global-datasets&quot;&gt;API-Key&lt;/a&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>dlgAbout</name>
+    <message>
+        <location filename="../about/ui_about.ui" line="0"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../about/ui_about.ui" line="0"/>
+        <source>Version:</source>
+        <translation>Version:</translation>
+    </message>
+    <message>
+        <location filename="../about/ui_about.ui" line="0"/>
+        <source>About </source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../about/ui_about.ui" line="0"/>
+        <source>Contributors</source>
+        <translation>Mitwirkende</translation>
+    </message>
+    <message>
+        <location filename="../about/ui_about.ui" line="0"/>
+        <source>Contact</source>
+        <translation>Kontakt</translation>
+    </message>
+    <message>
+        <location filename="../about/ui_about.ui" line="0"/>
+        <source>Change Log</source>
+        <translation>Änderungen</translation>
     </message>
 </context>
 </TS>
