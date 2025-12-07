05e00bc4b8b14b23af69f978031df28e64c99e07
Horst <horst.duester@kappasys.ch>
2025-12-07 22:39:19 +0100

    Management of download errors improved

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@
 import requests
 #from requests.auth import HTTPBasicAuth
 from qgis.PyQt.QtCore import QSettings,  Qt
-from qgis.PyQt.QtWidgets import QApplication
+from qgis.PyQt.QtWidgets import QApplication,  QMessageBox
                                                         
 
 
@@ -41,24 +41,37 @@ class Downloader:
                 url += f'&API_Key={api_key}'
                 self.download_stream(url,
                     out_path,
-                    progress_callback=lambda p: self.overall_progressbar.setValue(int(p))
+                    progress_callback=lambda p: int(p)
                 )
         
         return out_path
 
     def download_stream(self, url, out_path, progress_callback=None, cancel_callback=None):
         r = requests.get(url, stream=True)
-        total = int(r.headers.get('Content-Length', 0))
-        downloaded = 0
+        
+        if r.status_code != 200:
+            import xml.etree.ElementTree as ET
+            root = ET.fromstring(r.text)
+            error = root.text.split(':')
+            
+            QMessageBox.critical(
+                None,
+                "Error",
+                error[1],
+                (
+                    QMessageBox.StandardButton.Ok),
+            )
+        else:
+            downloaded = 0
 
-        with open(out_path, "wb") as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                if cancel_callback and cancel_callback():
-                    raise Exception("Download canceled")
+            with open(out_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if cancel_callback and cancel_callback():
+                        raise Exception("Download canceled")
 
-                if chunk:
-                    f.write(chunk)
-                    downloaded += len(chunk)
-                    
-                    if progress_callback and total:
-                        progress_callback(downloaded * 100 / total)
+                    if chunk:
+                        f.write(chunk)
+                        downloaded += len(chunk)
+                        
+                        if progress_callback:
+                            progress_callback(downloaded)
