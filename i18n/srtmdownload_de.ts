a9eedb781cf436769d03e0a13f3a278dced06b55
Horst <horst.duester@kappasys.ch>
2025-12-07 17:18:05 +0100

    German translation improved

--- i18n/srtmdownload_de.ts
+++ i18n/srtmdownload_de.ts
@@ -1,185 +1,184 @@
-<?xml version='1.0' encoding='utf-8'?>
-<TS version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_CH">
 <context>
     <name>About</name>
     <message>
-        <location filename="../about/do_about.py" line="38" />
+        <location filename="../about/do_about.py" line="38"/>
         <source>Author</source>
         <translation>Autor</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="39" />
+        <location filename="../about/do_about.py" line="39"/>
         <source>Contact</source>
         <translation>Kontakt</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="40" />
+        <location filename="../about/do_about.py" line="40"/>
         <source>Change Log</source>
-        <translation>Änderungsprotokoll</translation>
+        <translation>Änderungen</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="45" />
+        <location filename="../about/do_about.py" line="45"/>
         <source>&lt;p&gt;&lt;center&gt;&lt;b&gt;Author(s):&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</source>
         <translation>&lt;p&gt;&lt;center&gt;&lt;b&gt;Autor(en):&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="52" />
+        <location filename="../about/do_about.py" line="52"/>
         <source>Contact:
 </source>
-        <translation>Kontakt:
-</translation>
+        <translation>Kontakt:</translation>
     </message>
     <message>
-        <location filename="../about/do_about.py" line="55" />
+        <location filename="../about/do_about.py" line="55"/>
         <source>Plugin Resources:
 </source>
-        <translation>Plugin-Ressourcen:
-</translation>
+        <translation>Plugin Quellen:</translation>
     </message>
 </context>
 <context>
     <name>SrtmDownloader</name>
     <message>
-        <location filename="../srtm_downloader.py" line="67" />
+        <location filename="../srtm_downloader.py" line="67"/>
         <source>&amp;SRTM Downloader</source>
-        <translation>&amp;SRTM-Downloader</translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader.py" line="165" />
+        <location filename="../srtm_downloader.py" line="165"/>
         <source>SRTM Downloader</source>
-        <translation>SRTM-Downloader</translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>SrtmDownloaderDialogBase</name>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="158" />
-        <location filename="../srtm_downloader_dialog_base.py" line="150" />
+        <location filename="../srtm_downloader_dialog_base.py" line="165"/>
+        <location filename="../srtm_downloader_dialog_base.py" line="157"/>
         <source>Box out of covered area</source>
-        <translation>Box außerhalb des überdachten Bereichs</translation>
+        <translation>Box außerhalb des gedeckten Bereichs</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="151" />
+        <location filename="../srtm_downloader_dialog_base.py" line="158"/>
         <source>The area you have defined is completely outside the area covered by the SRTM tiles. </source>
-        <translation>Das von Ihnen definierte Gebiet liegt vollständig außerhalb des von den SRTM-Kacheln abgedeckten Bereichs.</translation>
+        <translation>Der von Ihnen definierte Bereich liegt vollständig außerhalb des von den SRTM-Kacheln abgedeckten Bereichs.</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="159" />
+        <location filename="../srtm_downloader_dialog_base.py" line="166"/>
         <source>The area you have defined is partly outside the area covered by the SRTM tiles. Do you like to continue?</source>
-        <translation>Das von Ihnen definierte Gebiet liegt teilweise außerhalb des von den SRTM-Kacheln abgedeckten Bereichs. Möchten Sie fortfahren?</translation>
+        <translation>Der von Ihnen definierte Bereich liegt teilweise außerhalb des von den SRTM-Kacheln abgedeckten Gebiets. Möchten Sie fortfahren?</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.py" line="217" />
+        <location filename="../srtm_downloader_dialog_base.py" line="224"/>
         <source>Open Directory</source>
         <translation>Verzeichnis öffnen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>SRTM Downloader</source>
-        <translation>SRTM-Downloader</translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>Load Image to QGIS</source>
-        <translation>Bild in QGIS laden</translation>
+        <translation>DEM in QGIS laden</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>Download</source>
-        <translation>Herunterladen</translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>Output-Path:</source>
-        <translation>Ausgangs-Pfad:</translation>
+        <translation>Ausgabepfad</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>[empty for temporary files]</source>
-        <translation>[leer für temporäre Dateien]</translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>...</source>
-        <translation>...</translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>DEM-Type:</source>
-        <translation>DEM-Type:</translation>
+        <translation>DEM-Typ:</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>South</source>
         <translation>Süd</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>Set canvas extent</source>
-        <translation>Leinwandausdehnung festlegen</translation>
+        <translation>Kartenausdehnung setzen</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>East</source>
         <translation>Ost</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>North</source>
-        <translation>Norden</translation>
+        <translation>Nord</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>West</source>
         <translation>West</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
         <source>Request API-Key from OpenTopography Data</source>
-        <translation>API-Schlüssel von OpenTopography-Daten anfordern</translation>
+        <translation>API-Schlüssel von OpenTopography Data anfordern</translation>
     </message>
     <message>
-        <location filename="../srtm_downloader_dialog_base.ui" line="0" />
-        <source>&lt;a href="https://opentopography.org/blog/introducing-api-keys-access-opentopography-global-datasets"&gt;API-Key&lt;/a&gt;</source>
-        <translation>&lt;a href="https://opentopography.org/blog/introducing-api-keys-access-opentopography-global-datasets"&gt;API-Schlüssel&lt;/a&gt;</translation>
+        <location filename="../srtm_downloader_dialog_base.ui" line="0"/>
+        <source>&lt;a href=&quot;https://opentopography.org/blog/introducing-api-keys-access-opentopography-global-datasets&quot;&gt;API-Key&lt;/a&gt;</source>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>dlgAbout</name>
     <message>
-        <location filename="../about/ui_about.ui" line="0" />
+        <location filename="../about/ui_about.ui" line="0"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../about/ui_about.ui" line="0" />
+        <location filename="../about/ui_about.ui" line="0"/>
         <source>Version:</source>
         <translation>Version:</translation>
     </message>
     <message>
-        <location filename="../about/ui_about.ui" line="0" />
+        <location filename="../about/ui_about.ui" line="0"/>
         <source>About </source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../about/ui_about.ui" line="0" />
+        <location filename="../about/ui_about.ui" line="0"/>
         <source>Contributors</source>
         <translation>Mitwirkende</translation>
     </message>
     <message>
-        <location filename="../about/ui_about.ui" line="0" />
+        <location filename="../about/ui_about.ui" line="0"/>
         <source>Contact</source>
         <translation>Kontakt</translation>
     </message>
     <message>
-        <location filename="../about/ui_about.ui" line="0" />
+        <location filename="../about/ui_about.ui" line="0"/>
         <source>Change Log</source>
-        <translation>Änderungsprotokoll</translation>
+        <translation>Änderungen</translation>
     </message>
 </context>
 </TS>
